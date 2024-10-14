234379e957626aaaf34a362b2662254a26ceee98
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-10-14 22:37:58 +0900

    feat: add photo

--- src/pages/book/PhotoPage.tsx
+++ src/pages/book/PhotoPage.tsx
@@ -1,18 +1,99 @@
 import styled from "@emotion/styled";
 import { InfoHeader } from "@/widgets";
+import { useState } from "react";
+
 const MainContainer = styled.div`
-    background-color: #FFFCAD;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 100px;
+  background-color: #FFFCAD;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 100px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-self: flex-start;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 20px;
+`;
+
+const TitleSubContainer = styled.div`
+  display: flex;
+  align-self: flex-start;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-left: 20px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const ImageContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+const AddImageBlock = styled.label<{ hasImage: boolean }>`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${(props) => (props.hasImage ? "0" : "100px")};
+  background-color: ${(props) => (props.hasImage ? "transparent" : "#d9d9d9")};
+  border-radius: 10px;
+  color: white;
+  overflow: hidden;
+  position: relative;
+`;
+
+const HiddenInput = styled.input`
+  display: none;
 `;
 
 const PhotoPage = () => {
+  const [images, setImages] = useState<string[]>(Array(10).fill("")); // 10개의 빈 이미지 슬롯을 준비
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const newImages = [...images];
+        newImages[index] = reader.result as string;
+        setImages(newImages);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <MainContainer>
-        <InfoHeader type="나만의 동화 만들기"/>
-        
+      <InfoHeader type="나만의 동화 만들기" />
+      <TitleContainer>OO이의 동화책에 들어갈 사진을 골라주세요!</TitleContainer>
+      <TitleSubContainer>사진 업로드 하러 가기</TitleSubContainer>
+      <ImageContainer>
+        {images.map((image, index) => (
+          <AddImageBlock key={index} htmlFor={`upload-${index}`} hasImage={!!image}>
+            {image ? <Image src={image} alt={`Uploaded ${index}`} /> : "?"}
+            <HiddenInput
+              type="file"
+              id={`upload-${index}`}
+              accept="image/*"
+              capture="environment"
+              onChange={(e) => handleImageChange(e, index)}
+            />
+          </AddImageBlock>
+        ))}
+      </ImageContainer>
     </MainContainer>
   );
 };
