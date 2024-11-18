47ffa4a473ba533f996fac83bd0c2e11502035db
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-11-18 17:46:38 +0900

    feat: implement record

--- src/entities/element/button/Record.tsx
+++ src/entities/element/button/Record.tsx
@@ -1,57 +1,54 @@
 import { useRef, useState } from "react";
 import { RecordIcon } from "./Button"
 import { BookService } from "@/shared/hooks/services/BookService";
+import { ReactMediaRecorder } from "react-media-recorder";
 
 export const Record = () => {
     const [recording, setRecording] = useState(false);
-    const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-    const mediaRecorder = useRef<MediaRecorder | null>(null);
-    const chunks = useRef<Blob[]>([]);
+    const [text, setText] = useState('');
     const record = BookService().record;
 
 
-    const handleStartRecording = async () => {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        
-        // 원하는 형식 지정 (예: 'audio/webm', 'audio/ogg' 등)
-        mediaRecorder.current = new MediaRecorder(stream, { mimeType: 'audio/ogg' });
-        chunks.current = [];
-        
-        mediaRecorder.current.ondataavailable = (event) => {
-            if (event.data.size > 0) chunks.current.push(event.data);
-        };
-        
-        mediaRecorder.current.onstop = () => {
-            const audioBlob = new Blob(chunks.current, { type: 'audio/ogg' }); // webm 형식으로 설정
-            setAudioBlob(audioBlob);
-        };
-        
-        mediaRecorder.current.start();
-        setRecording(true);
-    };
-
-    const handleStopRecording = () => {
-        mediaRecorder.current?.stop();
-        setRecording(false);
-        handleRecord();
-        console.log('stop');
-    };
-    const handleRecord = async () => {
-        if (!audioBlob) return;
-        const formData = new FormData();
-        formData.append('audio', audioBlob);
-        const data = await record({file: formData});
-        console.log(data);
-    }
+    
     return (
         <>
-            <RecordIcon onClick={recording ? handleStopRecording : handleStartRecording}/>
-            
-            {audioBlob && (
-                <a href={URL.createObjectURL(audioBlob)} download="recording.ogg">
-                    Download Recording
-                </a>
-            )}
+            {/* <RecordIcon onClick={recording ? handleStopRecording : handleStartRecording}/> */}
+            <>
+                {recording ? 'Recording...' : 'Click to Start Recording'}
+            </>
+            <ReactMediaRecorder
+                audio
+                render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+                    <div>
+                        <p>{status}</p>
+                        <RecordIcon onClick={() => {
+                            if (!recording) {
+                                setRecording(true);
+                                startRecording();
+                            } else {
+                                setRecording(false);
+                                stopRecording();
+                                if (mediaBlobUrl) {
+                                    fetch(mediaBlobUrl)
+                                    .then(res => res.blob())
+                                    .then(blob => {
+                                        const file = new File([blob], 'audio.mp3', {
+                                            type: 'audio/mp3',
+                                        });
+                                        console.log(file);
+                                        const formData = new FormData();
+                                        formData.append('file', file);
+                                        record({ file: formData });
+                                    });
+                                }
+                            }
+                        }} />
+                        {recording ? 'Start Recording' : 'Recording...'}
+                        <br />
+                        <audio src={mediaBlobUrl} controls />
+                    </div>
+                )}
+            />
         </>
     )
 }
--- src/shared/hooks/services/BookService.tsx
+++ src/shared/hooks/services/BookService.tsx
@@ -1,18 +1,14 @@
 import { AxiosResponse } from "axios";
-import { API, setAccess, storeAccess } from "@/shared";
+import { API } from "@/shared/configs/axios";
 
 export const BookService = () => {
 
     const record = async (body: Book.BookRecordReq) => {
         const { data } = (await API.post(
-            "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=Eng", 
-            body, {
-            headers: {
-                'Content-Type': 'application/octet-stream',
-                'X-NCP-APIGW-API-KEY-ID': import.meta.env.VITE_CLOVA_CLIENT_ID,
-                'X-NCP-APIGW-API-KEY': import.meta.env.VITE_CLOVA_CLIENT_SECRET
-            }}
-        )) as AxiosResponse<Book.BookRecordRes>;
+            "/api/character",
+            body
+        )) as AxiosResponse<User.SignUpResDto>;
+        console.log('data: ', data);
         return data;
     };
 
